5d89ac0618dae656430a8376540c946cecf82574
Michael Kinney <mkinney91@gmail.com>
2020-07-10 15:10:16 -0400

    Michael kinney (#1)
    * final commit
    
    * final push

--- exercise2/Types.java
+++ exercise2/Types.java
@@ -1,6 +1,6 @@
 package exercise2;
 
-class Types {
+//class Types {
 //    This class has syntax errors, that will prevent your other classes from running.  So... work on this file first to resolve the issues.
 //    boolean number;
 //    const myArray = [3,4,5,6,76,12,2];
@@ -14,4 +14,34 @@ class Types {
 //    public static void main() {
 //        System.out.println("There are variables that have been declared in this Accessible class.  Fix the code so that all the variables can be accessed by the main method.  print out the values of every variable once the code has been refactored.");
 //    }
+//}
+
+
+public class Types {
+    //    This class has syntax errors, that will prevent your other classes from running.  So... work on this file first to resolve the issues.
+    int number;
+    int[] myArray;
+    String movieA;
+    String movieB;
+    char male;
+    char female;
+
+    //the above instance variables have mistakes with their data types.  Fix the data types so the main method correctly displays the value of all of the variables
+
+    public Types(int number, int[] myArray, String movieA, String movieB, char male, char female) {
+        this.number = number;
+        this.myArray = myArray;
+        this.movieA = movieA;
+        this.movieB = movieB;
+        this.male = male;
+        this.female = female;
+    }
+
+    public static void main(String args[]) {
+
+        Types t = new Types(47, new int[]{3,4,5,6,76,12,2}, "Hary and the Hendersons", "Star Wars", 'm', 'f');
+        System.out.println("There are variables that have been declared in this Accessible class.  Fix the code so that all the variables can be accessed by the main method.  print out the values of every variable once the code has been refactored.");
+        System.out.println("You've watched " + t.movieB + " " + t.number + " times." );
+        System.out.println("Of everyone who saw " + t.movieA + " on opening weekend, 52% were " + t.male + ", and 48% were " + t.female + "." );
+    }
 }
--- udemy_exercises/bank/Bank.java
+++ udemy_exercises/bank/Bank.java
@@ -1,8 +1,112 @@
-package udemy_exercises.bank;
+package bank;
+
+        import java.util.ArrayList;
+
 
 public class Bank {
-    
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.newCustomer(customerName, initialAmount);
+        }
+        return false;
+
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.addCustomerTransaction(customerName, amount);
+        }
+        return false;
+    }
+
+    private Branch findBranch(String branchName) {
+        for (int i = 0; i < branches.size(); i++) {
+            Branch checkedBranch = this.branches.get(i);
+            if (checkedBranch.getName().equals(branchName)) {
+                return checkedBranch;
+            }
+        }
+        return null;
+    }
+
+    public boolean listCustomers(String branchName, boolean showTransactions) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            System.out.println("Customer details for branch" + branch.getName() + ":");
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for (int i = 0; i < branchCustomers.size(); i++) {
+                Customer branchCustomer = branchCustomers.get(i);
+                System.out.println("Customer: " + branchCustomer.getName() + "[" + (i + 1) + "]");
+                if (showTransactions) {
+                    System.out.println("Transactions: ");
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for (int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + (j + 1) + "] Amount: " + transactions.get(j));
+                    }
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Bank bank = new Bank("Bank of America");
+
+        if (bank.addBranch("Plano")) {
+            System.out.println("Plano branch created.");
+        }
+
+        bank.addCustomer("Plano", "Mike", 23.45);
+        bank.addCustomer("Plano", "Jim", 1120.33);
+        bank.addCustomer("Plano", "Dwight", 2.19);
+
+        bank.addBranch("Irving");
+        bank.addCustomer("Irving", "Stanely", 1200.22);
+
+        bank.addCustomerTransaction("Plano", "Mike", 200.25);
+        bank.addCustomerTransaction("Plano", "Mike", 189.64);
+        bank.addCustomerTransaction("Plano", "Jim", -30.54);
+
+        bank.listCustomers("Plano", true);
+        bank.listCustomers("Irving", true);
+
+        bank.addBranch("Frisco");
+
+        if (!bank.addCustomer("Frisco", "Ryan", 16.34)) {
+            System.out.println("Error. Frisco branch does not exist.");
+        }
+        if (!bank.addBranch("Plano")) {
+            System.out.println("Plano branch already exists.");
+        }
+        if (!bank.addCustomerTransaction("Plano", "Pam", 46.78)) {
+            System.out.println("Customer does not exist.");
+        }
+        if (!bank.addCustomer("Plano", "Mike", 31.67)) {
+            System.out.println("Customer Mike already exists.");
+        }
+    }
 }
+
 
 
 /*
